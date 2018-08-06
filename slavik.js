6e18ac6ec681802697cf7f5fbea4b02204d3bf2d
slava kakava <slawcka@gmail.com>
2018-08-06 16:53:49 +0300

    cat, cosmos working

--- slavik.js
+++ slavik.js
@@ -28,4 +28,66 @@ function movingNav() {
     buttons.forEach(e => e.addEventListener('mouseenter', movingEffect));
 
 }
+
+
+function movingBg(){
+    const cosmosBg = document.querySelector('.intro');
+
+    function bgAnimate(e) {
+        let {
+            offsetX: x,
+            offsetY: y
+        } = e;
+        if (this !== e.target) {
+            x = x + e.target.offsetLeft;
+            y = y + e.target.offsetTop;
+        }
+        const xmove = (x * 0.002) + 50;
+        const ymove = (y * 0.008) + 50;
+        console.log(xmove);
+        cosmosBg.style.backgroundPosition = `${xmove}% ${ymove}%`
+    }
+    cosmosBg.addEventListener('mousemove', bgAnimate, false);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+movingBg();
 movingNav();
+
+// const walk=400;
+// const intro= document.querySelector('.intro');
+// const cat= document.querySelector('.cat');
+
+// function catRotate(e){
+//   const {offsetWidth:width, offsetHeight:height} = intro;
+//   let {offsetY:y, offsetX:x} = e;
+
+//   let wide= width/2;
+//   let heit=height/2;
+//   if (this !== e.target ) {
+//     x = x + (e.target.offsetLeft - 20);
+//       y = y + e.target.offsetTop + 26;
+//   }
+//     var centrox= x-wide;
+//   var centroy=heit-y;
+//   x = Math.round((centroy * 0.06)+10);
+//   y =Math.round ((centrox * 0.06));
+//   // width 800 height 400
+//   // top left x10 y-10 bottom right x-10 y10
+//   const xWalk = Math.round((x / width * walk) - (walk / 2));
+//   const yWalk = Math.round((y / height * walk) - (walk / 2));
+//   cat.style.transform = `rotateX(${x}deg) rotateY(${y}deg)`;
+// }
+
+
+// intro.addEventListener('mousemove',catRotate,true);
